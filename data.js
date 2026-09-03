d56a84f19cecbd83dbd4741979af700e0cad89a8
GermanTheSpanish <159256975+GermanTheSpanish@users.noreply.github.com>
2026-09-03 23:55:35 +0200

    ITERION v0.13.1 UX timing and hand animation config

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
   root.IterionData=api;
 })(typeof globalThis!=='undefined'?globalThis:this,function(){
   return Object.freeze({
-    VERSION:'0.13.0',
+    VERSION:'0.13.1',
     ENGINE_VERSION:'0.10.3-accumulator',
     TARGETS:[20,100,500,2500,10000],
     MAX_PLACEMENTS:7,
@@ -15,7 +15,10 @@
     SHAKE_SWITCHES:2,
     SHAKE_WINDOW_MS:460,
     SHAKE_COOLDOWN_MS:420,
-    OUTCOME_SCREEN_DELAY_MS:1100,
+    OUTCOME_SCREEN_DELAY_MS:550,
+    REROLL_BLACK_MS:500,
+    HAND_REVEAL_STAGGER_MS:75,
+    DRAW_BLACK_MS:180,
     STARTING_COINS:5,
     MARKET_AFTER_PLACEMENT:3
   });
