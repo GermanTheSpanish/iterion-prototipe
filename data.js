1a98b50d89f91f83afd231ec8ddc2721225c501f
GermanTheSpanish <159256975+GermanTheSpanish@users.noreply.github.com>
2026-09-12 00:03:04 +0200

    Release MONOID v0.28.4

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
   root.IterionData=api;
 })(typeof globalThis!=='undefined'?globalThis:this,function(){
   return Object.freeze({
-    VERSION:'0.28.3',
+    VERSION:'0.28.4',
     ENGINE_VERSION:'0.14.0-split',
     BIFURCATION_ENABLED:true,
     TARGETS:[
