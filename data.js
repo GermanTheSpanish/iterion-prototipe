2649ce050f031124dbd88348b6e1bae180f11402
GermanTheSpanish <159256975+GermanTheSpanish@users.noreply.github.com>
2026-09-21 20:06:27 +0100

    Bump data.js for v0.42.4 telemetry release

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
   root.IterionData=api;
 })(typeof globalThis!=='undefined'?globalThis:this,function(){
   return Object.freeze({
-    VERSION:'0.42.3',
+    VERSION:'0.42.4',
     ENGINE_VERSION:'0.15.0-l-split',
     BIFURCATION_ENABLED:true,
     TARGETS:[
