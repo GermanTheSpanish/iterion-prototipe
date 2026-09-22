c9e1aa1279b4c557c68547f30dbc034938f648b4
GermanTheSpanish <159256975+GermanTheSpanish@users.noreply.github.com>
2026-09-22 12:46:01 +0100

    Package cascade repeat compression v0.42.6

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
   root.IterionData=api;
 })(typeof globalThis!=='undefined'?globalThis:this,function(){
   return Object.freeze({
-    VERSION:'0.42.5',
+    VERSION:'0.42.6',
     ENGINE_VERSION:'0.15.0-l-split',
     BIFURCATION_ENABLED:true,
     TARGETS:[
