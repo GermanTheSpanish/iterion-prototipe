96be7778c09685bb74b2c0737c2bf750fe5900b2
GermanTheSpanish <159256975+GermanTheSpanish@users.noreply.github.com>
2026-09-26 10:16:47 +0100

    Fix topology lifecycle fixture geometry

--- tests/v045-topology-lifecycle.test.js
+++ tests/v045-topology-lifecycle.test.js
@@ -9,8 +9,8 @@ function placed(g,id,x,y,rr,pieceId){
 }
 function elbow(g){
   const s=g.state();
-  s.pieces=[placed(g,'d3-4',6,8,0,1),placed(g,'d3-5',2,8,0,2),placed(g,'d2-4',8,10,1,3)];
-  s.placedTileIds=['d3-4','d3-5','d2-4'];
+  s.pieces=[placed(g,'d3-4',6,8,0,1),placed(g,'d3-5',4,8,2,2),placed(g,'d4-6',8,10,1,3)];
+  s.placedTileIds=['d3-4','d3-5','d4-6'];
   s.running=false;s.cleared=false;s.blocked=false;s.needsReroll=false;s.shopOpen=false;s.pendingCircuit=null;s.pendingModPlacement=null;
   return s
 }
